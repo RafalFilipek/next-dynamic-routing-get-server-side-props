48ea6f6317f3de9e988d5581ad37b5de90ac3b5b
Rafau <rafal.filipek@gmail.com>
2020-04-27 16:42:08 +0200

    Add files via upload

---
+++ pages/[...path].js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default (props) => {
+  return (
+    <div>
+      <Link href="/">
+        <a>index</a>
+      </Link>
+      <pre>{JSON.stringify(props.data)}</pre>
+    </div>
+  );
+};
+
+export const getServerSideProps = ({ req, res, ...rest }, next) => {
+  console.log(rest);
+  return {
+    props: {
+      data: 2,
+    },
+  };
+};
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Link from "next/link";
+
+export default (props) => {
+  return (
+    <div>
+      <Link href="/foo">
+        <a>foo</a>
+      </Link>
+      <pre>{JSON.stringify(props.data)}</pre>
+    </div>
+  );
+};
+
+export const getServerSideProps = ({ req, res, ...rest }, next) => {
+  console.log(req.url);
+  return {
+    props: {
+      data: 2,
+    },
+  };
+};
